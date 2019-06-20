411a6d248bd0835f4628568c30379ead9fd57c89
John Haynes <john.andrew.haynes@gmail.com>
2019-06-20 16:06:27 +0200

    experiment

--- src/Main.hs
+++ src/Main.hs
@@ -13,6 +13,59 @@ import Network.HTTP.Client.TLS      (mkManagerSettings)
 import Network.Connection           (TLSSettings (TLSSettingsSimple))
 import System.Environment           (getArgs)
 
+import Control.Monad.IO.Class
+import Control.Exception.Safe    (handleAnyDeep, handleAny, MonadCatch, MonadThrow, SomeException, throw, throwString)
+import Data.ByteString.Char8     (unpack)
+import Data.ByteString.Lazy      (ByteString)
+import Data.CaseInsensitive      (mk)
+import Network.HTTP.Client       (Manager, Request, Response, httpLbs, parseRequest, responseStatus, responseHeaders)
+import Network.HTTP.Types.Status (statusCode)
+import Safe                      (headMay)
+
+class Monad m => Crawler m where
+
+    new         :: m Manager
+
+    parse       :: Url -> m Request
+    parseEither :: Url -> m (Either SomeException Request)
+
+    fetch       :: Manager -> Request -> m (Response ByteString)
+    fetchEither :: Manager -> Request -> m (Either SomeException (Response ByteString))
+
+    checked     :: m a -> m (Either SomeException a)
+    unchecked   :: m (Either SomeException a) -> m a
+
+instance Crawler IO where
+
+    new = newManager (mkManagerSettings (TLSSettingsSimple True False False) Nothing) -- tlsManagerSettings
+
+    parse (Url u) = parseRequest u
+    parseEither u = handleAny (pure . Left) (Right <$> parse u)
+
+    fetch       http req = httpLbs req http
+    fetchEither http req = handleAny (pure . Left) (Right <$> (fetch http req))
+
+    checked ma = handleAny (pure . Left) (Right <$> ma)
+
+    unchecked mea = do
+        ea <- mea
+        case ea of
+            Left e -> throw e
+            Right a -> pure a
+
+wrap ma = handleAny (pure . Left) (Right <$> ma)
+
+main = do
+
+    x <- checked $ do
+        man  <- new
+        req  <- parse (Url "https://github.com")
+        resp <- fetch man req
+        pure resp
+
+    print x
+
+{-
 main :: IO ()
 main = do
 
@@ -39,3 +92,4 @@ accept d url =
         Just x -> x == d
         Nothing -> False
 
+-}
